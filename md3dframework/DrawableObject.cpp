8eb7e40ce1d1604446e194699edb31ebba0f3bd1
mvdgaag <maarten.van.der.gaag@gmail.com>
2015-10-05 09:03:07 +0200

    .

--- md3dframework/DrawableObject.cpp
+++ md3dframework/DrawableObject.cpp
@@ -50,12 +50,6 @@ void DrawableObject::Init(Mesh* inMesh, PixelShader* inPixelShader, VertexShader
 }
 
 
-
-const float halton23x[8] = {	1.0f/2.0f, 1.0f/4.0f, 3.0f/4.0f, 1.0f/8.0f,
-								5.0f/8.0f, 3.0f/8.0f, 7.0f/8.0f, 1.0f/16.0f };
-const float halton23y[8] = {	1.0f/3.0f, 2.0f/3.0f, 1.0f/9.0f, 4.0f/9.0f,
-								7.0f/9.0f, 2.0f/9.0f, 5.0f/9.0f, 8.0f/9.0f };
-
 void DrawableObject::Draw()
 {
 	// check if everything is in place
@@ -80,6 +74,10 @@ void DrawableObject::Draw()
 	theFramework.SetTextureAndSampler(mMaterialTexture, mMaterialSampler, 2);
 
 	// todo: check if this leads to proper motion vectors
+	const float halton23x[8] = {	1.0f/2.0f, 1.0f/4.0f, 3.0f/4.0f, 1.0f/8.0f,
+									5.0f/8.0f, 3.0f/8.0f, 7.0f/8.0f, 1.0f/16.0f };
+	const float halton23y[8] = {	1.0f/3.0f, 2.0f/3.0f, 1.0f/9.0f, 4.0f/9.0f,
+									7.0f/9.0f, 2.0f/9.0f, 5.0f/9.0f, 8.0f/9.0f };
 	int idx = theFramework.GetFrameID() % 8;
 	mConstantBufferData.offsets.z = mConstantBufferData.offsets.x; // previous x
 	mConstantBufferData.offsets.w = mConstantBufferData.offsets.y; // previous y
