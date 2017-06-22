d459a6f44422f7029474a0224dd68733058cd6c7
mvdgaag <maarten.van.der.gaag@gmail.com>
2017-06-23 00:12:08 +0200

    Fixed to include new every frame constant buffer #0 and move material to #1

--- TerrainEditor/Shaders/TerrainFragmentShader.hlsl
+++ TerrainEditor/Shaders/TerrainFragmentShader.hlsl
@@ -14,13 +14,23 @@ SamplerState cSurfaceSampler : register(s2);
 #define HAS_SURFACE_MAP	(1 << 2)
 
 
-cbuffer cConstantData : register(b0)
+cbuffer cEveryFrame : register(b0)
+{
+	matrix viewMatrix;
+	matrix projectionMatrix;
+	matrix viewProjectionMatrix;
+	matrix inverseProjectionMatrix;
+	float4 frameData;								// jitter_offset.xy, frameID, time
+};
+
+
+cbuffer cConstantData : register(b1)
 {
 	float4 cDiffuse;
 	float4 cSurface;
 	uint cFlags;
 	float4 cPadding;
-}
+};
 
 
 struct PS_INPUT
